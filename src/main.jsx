098613972bf9c6ae0c2efcc291cf34c4d42e5b3e
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-08 11:16:06 +0700

    Setup login

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,16 @@ import React,{useState, createContext, useContext} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+function App() {
+  const [isAuth, setIsAuth] = useState(true);
+  const handleAuth = () => setIsAuth(!isAuth)
+  return (
+    <div className='App'>
+      <h1>Welcome.. {!isAuth? 'Guest' : 'User'}</h1>
+      <button onClick={handleAuth}>{!isAuth? 'Login': 'Logout'}</button>
+    </div>
+  )
+}
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<div>Vite React Template</div>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
