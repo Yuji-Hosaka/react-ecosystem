0de11d06015329927f91045d84e1306a251f2971
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-08 16:57:01 +0700

    Button mui

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,37 @@
-import React,{useState, createContext, useContext} from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Button from '@mui/material/Button';
 
+function App() {
+  return (
+    <div>
+      <Button variant='text'>button</Button>
+      <Button variant='contained'>button</Button>
+      <Button variant='contained' disabled>
+        button
+      </Button>
+      <Button variant='outlined'>button</Button>
 
+      <Button variant='contained' color='primary'>
+        button
+      </Button>
+      <Button variant='contained' color='secondary'>
+        button
+      </Button>
+      <Button variant='contained' color='success'>
+        button
+      </Button>
+      <Button
+        variant='contained'
+        color='error'
+        onClick={() => alert('Clicked')}
+        sx={{ padding: '10px', marginTop: '20px', borderRadius: '10px' }}
+      >
+        button
+      </Button>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<div>Vite React Template</div>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
