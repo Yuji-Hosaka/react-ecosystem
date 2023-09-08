2ec107a35bee22e504c96b53f451d036052c741c
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-08 11:33:37 +0700

    Finish login

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,34 @@
-import React,{useState, createContext, useContext} from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React, { useState, createContext, useContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// สร้าง <AuthContext/> สำหรับ Provide isAuth,handleAuth ให้ <App/>
 
-function App() {
+const AuthContext = createContext();
+
+// function AuthContextProvider(props) {
+//   return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+// }
+
+function AuthContextProvider({ children }) {
   const [isAuth, setIsAuth] = useState(true);
-  const handleAuth = () => setIsAuth(!isAuth)
-  return (
-    <div className='App'>
-      <h1>Welcome.. {!isAuth? 'Guest' : 'User'}</h1>
-      <button onClick={handleAuth}>{!isAuth? 'Login': 'Logout'}</button>
-    </div>
-  )
+  const handleAuth = () => setIsAuth(!isAuth);
+
+  // const shareObj = { isAuth, handleAuth };
+  return <AuthContext.Provider value={{ isAuth, handleAuth }}>{children}</AuthContext.Provider>;
 }
 
+function App() {
+  const { isAuth, handleAuth } = useContext(AuthContext);
+  return (
+    <div className='App'>
+      <h1>Welcome.. {!isAuth ? 'Guest' : 'User'} </h1>
+      <button onClick={handleAuth}>{!isAuth ? 'Login' : 'Logout'}</button>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
