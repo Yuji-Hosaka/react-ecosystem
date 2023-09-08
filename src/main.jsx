2a91d6e18788f06a5bb30c2e2cc6c3c9c0d475d9
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-08 16:27:18 +0700

    refector multi folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React,{useState, createContext, useContext} from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<div>Vite React Template</div>
-)
+// npm i react-router-dom
