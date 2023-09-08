883613061da2e4b70d8d444189088eb240ef94df
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-08 10:00:44 +0700

    1 push

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App () {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const stylesObj = {
+    backgroundColor: isDarkMode ? 'black' : 'white',
+    color: isDarkMode? 'white' : 'black',
+  };
+
+  const handleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+
+  return (
+    
+    <div className= 'App' style={stylesObj}>
+      <h1>
+        Theme App
+        </h1>
+        <button onClick={handleToggleTheme}>Switch Theme</button>
+      </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+
